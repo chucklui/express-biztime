1db4cc05635f870fffd886a9a7afa04a83bbacb7
hectorchu~ <hector.chu828@gmail.com>
2022-02-17 12:32:17 -0800

    Completed all CRUD operations and finished code review edits in companies

--- routes/companies.js
+++ routes/companies.js
@@ -1,39 +1,99 @@
 "use strict";
 
 const express = require("express");
-const { route } = require("../app");
 const db = require("../db");
-const { NotFoundError } = require("./expressError");
+const { NotFoundError } = require("../expressError");
 const router = new express.Router();
 
-route.get('/', async function (req, res, next) {
-  const result = await db.query(`SELECT code, name FROM companies`);
-  const companies = result.rows;
-  return res.json({ companies });
+/** Queries all records in companies table,
+ *  returns list of companies
+ * {companies: [{code, name}, ...]}
+ */
+
+router.get('/', async function (req, res, next) {
+    const result = await db.query(
+        `SELECT code, name 
+            FROM companies
+            ORDER BY code`);
+    const companies = result.rows;
+    return res.json({ companies });
 });
 
-route.get('/:code', async function (req, res, next) {
-  const code = req.params.code;
-  const result = await db.query(
-    `SELECT code, name, code 
-    FROM companies 
-    WHERE code = $1`,
-    [code]);
-  const company = result.rows[0];
-  return res.json({ company });
+/** Queries single company with code pk in companies table,
+ *  returns record or 404 
+ * {company: {code, name, description}}
+ */
+
+router.get('/:code', async function (req, res, next) {
+    const code = req.params.code;
+    const result = await db.query(
+        `SELECT code, name, description
+            FROM companies 
+            WHERE code = $1`,
+        [code]);
+
+    if (result.rows.length === 0) {
+        throw new NotFoundError("Company not found");
+    }
+    const company = result.rows[0];
+    return res.json({ company });
 });
 
-route.post('/', async function(req, res, next){
-  const {code, name, description} = req.body;
-  const result = await db.query(
-    `INSERT INTO companies (code, name, description)
+/** Creates new record in companies table with { name, description }
+ * returns {company: {code, name, description}}
+ */
+router.post('/', async function (req, res, next) {
+    const { code, name, description } = req.body;
+    const result = await db.query(
+        `INSERT INTO companies (code, name, description)
            VALUES ($1, $2, $3)
            RETURNING code, name, description`,
-    [code, name, description]
-  );
-  const company = result.rows[0];
-  return res.status(201).json({ company });
+        [code, name, description]
+    );
+    const company = result.rows[0];
+    return res.status(201).json({ company });
 });
+
+/** Updates existing record in companies table with { name, description }
+ * returns updated record or 404
+ * {company: {code, name, description}}
+ */
+
+router.put("/:code", async function (req, res, next) {
+    const { name, description } = req.body;
+
+    const result = await db.query(
+        `UPDATE companies
+            SET name=$1,
+                description=$2
+            WHERE code=$3
+            RETURNING code, name, description`,
+        [name, description, req.params.code]);
+
+    if (result.rows.length === 0) {
+        throw new NotFoundError("Company not found");
+    };
+    const company = result.rows[0];
+    return res.json({ company });
+});
+
+/** Deletes record with code pk in companies table,
+ * returns {status: "deleted"} or 404
+ */
+
+router.delete("/:code", async function (req, res, next) {
+    const result = await db.query(
+        `DELETE FROM companies 
+            WHERE code=$1
+            RETURNING code`,
+        [req.params.code]
+    );
+
+    if (result.rows.length === 0) {
+        throw new NotFoundError("Company not found");
+    };
+    return res.json({ status: "deleted" });
+})
 
 
 
